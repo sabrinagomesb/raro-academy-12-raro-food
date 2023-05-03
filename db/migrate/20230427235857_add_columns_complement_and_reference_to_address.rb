f988823918df79b6b4279a44cbcb5af35737f6d3
Guilherme Andrade <andrade@rarolabs.com.br>
2023-05-03 09:01:02 -0300

    fix: adjust migration

--- db/migrate/20230427235857_add_columns_complement_and_reference_to_address.rb
+++ db/migrate/20230427235857_add_columns_complement_and_reference_to_address.rb
@@ -2,7 +2,7 @@
 
 class AddColumnsComplementAndReferenceToAddress < ActiveRecord::Migration[7.0]
   def change
-    change_table :addresses, bulk: true do
+    change_table :addresses, bulk: true do |t|
       t.string :reference, null: true, limit: 255, after: :zip_code
       t.string :complement, null: true, limit: 255, after: :reference
     end
