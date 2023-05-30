baf03d076cd0eb0ffdd3f70cc42f25af7abf5250
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-30 14:58:43 -0300

    feat: adiciona helpers ao order item mailer para utilizar formato moeda

--- app/mailers/order_item_mailer.rb
+++ app/mailers/order_item_mailer.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 class OrderItemMailer < ApplicationMailer
+  include ApplicationHelper
+  include ActionView::Helpers::NumberHelper
+
   default from: email_address_with_name('noreply@rarofood.com', 'RaroFood')
 
   def order_item_added(order_item)
     @order_item = order_item
     @dish = @order_item.dish
+    @dish_price = usa_currancy(@order_item.dish.unit_price)
+    @customer = @order_item.order.customer
     chef_email = @dish.chef.user.email
 
-    mail(to: chef_email, subject: 'Prato adicionado a um pedido')
+    mail(to: chef_email, subject: 'New dish order')
   end
 end
