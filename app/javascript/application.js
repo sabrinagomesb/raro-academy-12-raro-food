bc1f9d4af7066ebcafd08d5fbab0c19548693d6f
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-30 20:22:39 -0300

    feat: adiciona text actions ao projeto

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Entry point for the build script in your package.json
 import { Turbo } from "@hotwired/turbo-rails";
 import "./controllers";
+import "trix"
+import "@rails/actiontext"
 
 Turbo.session.drive = false;
