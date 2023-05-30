8b5a6e032c06c9468dc2fb839c58cefdc61c9bda
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-30 02:02:30 -0300

    feat: adiciona callback para mailer no  model order item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -11,19 +11,24 @@ class OrderItem < ApplicationRecord
   before_validation :set_unit_price
   after_destroy :update_order_total_price
   after_save :update_order_total_price
+  after_commit :send_email_to_chef
 
   delegate :active?, :unit_price, to: :dish, prefix: true
 
   scope :order_items_by_dishes_on_finished_orders, lambda { |dish_id|
-                                                     joins(:order, :dish).where(dishes: { id: dish_id })
-                                                                         .where(orders: { status: :finished })
-                                                   }
+    joins(:order, :dish).where(dishes: { id: dish_id })
+                        .where(orders: { status: :finished })
+  }
 
   def total_per_item
     amount * unit_price
   end
 
   private
+
+  def send_email_to_chef
+    OrderItemMailer.order_item_added(self).deliver_later
+  end
 
   def can_add_item?
     errors.add(:dish_id, 'is not an active and available item') unless dish.can_be_sold?
