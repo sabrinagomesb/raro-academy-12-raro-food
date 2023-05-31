d9002a39db7e0b5ebd7c1bf05b4250fb45721300
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-31 17:58:01 -0300

    refactor: insere variaveis de ambiente para acesso ao sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,5 @@ require "sidekiq"
 require "sidekiq/web"
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
-  # [user, password] == ["rarofood", "rarofood@r@r02023"]
   [user, password] == [ENV["SIDEKIQ_USERNAME"], ENV["SIDEKIQ_PASSWORD"]]
 end
